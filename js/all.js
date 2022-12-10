67ce9fae33243385ca7b23ce3ec33b7260054b6f
Fernanda leite <fernandacarolineleite360@gmail.com>
2022-12-10 19:31:03 -0300

    style: add style responsive

--- js/all.js
+++ js/all.js
@@ -1,1 +1,1 @@
-"use strict";var slideWorks=new Swiper(".slide-works",{slidesPerView:2.5,spaceBetween:45,breakpoints:{320:{slidesPerView:1.5,spaceBetween:20},1024:{slidesPerView:2.5,spaceBetween:40}}});AOS.init();
+"use strict";var slideWorks=new Swiper(".slide-works",{slidesPerView:1,spaceBetween:10,breakpoints:{720:{slidesPerView:1,spaceBetween:20},1024:{slidesPerView:2,spaceBetween:20},1366:{slidesPerView:2.5,spaceBetween:40}}});AOS.init();
--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,12 +1,16 @@
 var slideWorks = new Swiper(".slide-works", {
-  slidesPerView: 2.5,
-  spaceBetween: 45,
+  slidesPerView: 1,
+  spaceBetween: 10,
   breakpoints: {
-    320: {
-      slidesPerView: 1.5,
+    720: {
+      slidesPerView: 1,
       spaceBetween: 20
     },
     1024: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1366: {
       slidesPerView: 2.5,
       spaceBetween: 40,
     }
