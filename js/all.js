956093e244062826424c4422c3914ca47110d00a
Fernanda leite <fernandacarolineleite360@gmail.com>
2022-12-09 22:09:40 -0300

    feat: create section works

--- js/all.js
+++ js/all.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";var slideWorks=new Swiper(".slide-works",{slidesPerView:2.5,spaceBetween:45,breakpoints:{320:{slidesPerView:1.5,spaceBetween:20},1024:{slidesPerView:2.5,spaceBetween:40}}});
---
+++ js/scripts/main.js
@@ -0,0 +1,14 @@
+var slideWorks = new Swiper(".slide-works", {
+  slidesPerView: 2.5,
+  spaceBetween: 45,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.5,
+      spaceBetween: 20
+    },
+    1024: {
+      slidesPerView: 2.5,
+      spaceBetween: 40,
+    }
+  }
+});
